aee65f79f0d4f0456caac5c3308b7da62dfe2fa4
Yifei Huang <yfhuang@internal.synopsys.com>
2017-01-03 09:32:06 +0800

    laplace smoothing

--- mlaction/chap04_bayes/bayes.py
+++ mlaction/chap04_bayes/bayes.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 def loaddataset():
 	posttingglist = [
 		['my', 'dog', 'has','flea','problem', 'help', 'please'],
@@ -25,9 +28,42 @@ def setofwords2vec(vocablist, inputset):
 	return returnvec
 
 
-if __name__ == '__main__':
+def trainNB0(trainingmatrix, traningcategory):
+	numtraindocs = len(trainingmatrix)
+
+	#  vocab count
+	numwords = len(trainingmatrix[0])
+	pabusive = sum(traningcategory) / float(numtraindocs)
+	# p0num = np.zeros(numwords)
+	p0num = np.ones(numwords)
+	# p1num = np.zeros(numwords)
+	p1num = np.ones(numwords)
+	# p0denom = 0.0
+	p0denom = 2.0
+	# p1denom = 0.0
+	p1denom = 2.0
+	for i in range(numtraindocs):
+		if traningcategory[i] == 1:
+			p1num += trainingmatrix[i]
+			p1denom += sum(trainingmatrix[i])
+		else:
+			p0num += trainingmatrix[i]
+			p0denom += sum(trainingmatrix[i])
+	p1vect = p1num/p1denom
+	p1vect = np.log(p1vect)
+	p0vect = p0num/p0denom
+	p0vect = np.log(p0vect)
+	return p0vect, p1vect, pabusive
+
+
+
+
+
+
+def test():
 	listofposts, listclasses = loaddataset()
 	vocablist = createvocablist(listofposts)
 	print(vocablist)
 	wordvec = setofwords2vec(vocablist, listofposts[0])
 	print(wordvec)
+
