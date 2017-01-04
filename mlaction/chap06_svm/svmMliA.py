7800a4216560d3545600b039d92c06bbe7a6c4d9
easyfly007 <easyfly.huang@gmail.com>
2017-01-04 22:56:21 +0800

    add the data file for svm

---
+++ mlaction/chap06_svm/svmMliA.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import random
+
+def loaddataset(filename):
+	datamat = []
+	labelmat = []
+	f = open(filename)
+	for line in f.readlines():
+		items = line.strip().split('\t')
+		datamat.append([float(items[0]), float(items[1])] )
+		labelmat.append(float(items[2]))
+	return np.mat(datamat), np.mat(labelmat)
+
+def selectjrand(i, m):
+	j=i
+	while (j==i):
+		j = int(random.uniform(0, m))
+	return j
+
+def clipalpha(aj, high, low):
+	if aj >high:
+		aj = high
+	if aj < low:
+		aj = low
+	return aj
+
+
+
+
